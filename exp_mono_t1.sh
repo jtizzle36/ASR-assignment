41900c4eaf1d3dc2a4917b170c9efed3ee24801d
jchiiim <jennychim@gmail.com>
2018-03-07 09:30:53 +0000

    Update exp_mono_t1.sh

--- exp_mono_t1.sh
+++ exp_mono_t1.sh
@@ -2,11 +2,11 @@
 
 # Explore optimal number of Gaussian components
 
-for totgauss in {100..2000..100}
+for totgauss in {100..2500..100}
 do
   steps/train_mono.sh --totgauss $totgauss --nj 4 data/train_words data/lang_wsj exp/mono${totgauss}
   utils/mkgraph.sh --mono data/lang_wsj_test_bg exp/mono${totgauss} exp/mono${totgauss}/graph
   steps/decode.sh --nj 4 exp/mono${totgauss}/graph data/test_words exp/mono${totgauss}/decode_test
-  local/score_words.sh data/test_words exp/mono${totgauss}/graph exp/mono${totgauss}/decode_test/
+  local/score_words.sh data/test_words exp/mono${totgauss}/graph exp/mono${totgauss}/decode_test
   more exp/mono${totgauss}/decode_test/scoring_kaldi/best_wer >> my-local/exp_mono_results
 done
