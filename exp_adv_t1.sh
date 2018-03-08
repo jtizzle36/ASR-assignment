b33dcc87b312c0a1c3eecf8ed0db1da60d40a29d
jchiiim <jennychim@gmail.com>
2018-03-08 14:57:20 +0000

    Work in progress for 3a, need to test and train
    Finished alignment, data sampling; need to figure out how to test model

--- exp_adv_t1.sh
+++ exp_adv_t1.sh
@@ -2,32 +2,46 @@
 
 # Training by gender
 
-# Preparing data:
-# Filter directory by speaker gender; assumes speaker list available
-# Data from 120 male and 120 female speakers have been randomly selected
-#for gender in m f
-#do
-#  ./utils/subset_data_dir.sh --spk-list my-local/${gender} data/train_words data/train_${gender}
-#done
+# Using optimal parameters and alignment from previous question
+num_leaves=2700
+num_gauss=15000
+lang_dir=data/lang_wsj
+orig_model=exp/tri_t1_${num_leaves}_${num_gauss}
 
-# =================================== WORK IN PROGRESS ===================================
-# Using optimal parameters found in previous question
-num_leaves=2500 #<- REPLACE WITH PARAMETERS!
-num_gauss=15000 #<- REPLACE WITH PARAMETERS!
+# =================  PREPARING DATA AND TRAINING MODELS   ========================
+# Filter directory by speaker gender; assumes speaker list available in my-local
+# Data from speakers have been randomly selected as follows:
+# Training -> 120 male/120 female/60 male and 60 female
+# Test -> 8 male/8 female/4 male 4 female
 
-orig_model=exp_name=tri_t1_${num_leaves}_${num_gauss}
-algn_dir=${orig_model}_ali
-steps/align_si.sh --nj 4 ${data_dir} ${lang_dir} ${orig_model} ${algn_dir}
-
-# Start loop for gender
-for gender in m f
+for gender in m f mix
 do
+  train_list=${gender}_train
+  test_list=${gender}_test
+  exp_dir=exp/gender_${gender}
   data_dir=data/train_${gender}
-  lang_dir=data/lang_wsj
-  exp_dir=exp/gender_${exp_name}
-  ./steps/train_deltas.sh ${num_leaves} ${num_gauss} ${data_dir} ${lang_dir} ${algn_dir} ${exp_dir}
-  ./utils/mkgraph.sh data/lang_wsj_test_bg ${exp_dir} ${exp_dir}/graph
-  ./steps/decode.sh --nj 4 ${exp_dir}/graph data/test_words ${exp_dir}/decode_test
-  ./local/score_words.sh data/test_words ${exp_dir}/graph ${exp_dir}/decode_test
-  more ${exp_dir}/decode_test/scoring_kaldi/best_wer >> my-local/gender_results
+  algn_dir=${orig_model}_${gender}_ali
+  #./utils/subset_data_dir.sh --spk-list my-local/${train_list} data/train_words data/train_${gender}
+  #./utils/subset_data_dir.sh --spk-list my-local/${test_list} data/test_words data/test_${gender}
+  ./steps/align_si.sh --nj 4 ${data_dir} ${lang_dir} ${orig_model} ${algn_dir}
+  #./steps/train_deltas.sh ${num_leaves} ${num_gauss} ${data_dir} ${lang_dir} ${algn_dir} ${exp_dir}
+done
+
+# =============================== TESTING MODEL ==============================
+# =============================== WORK IN PROGRESS ==============================
+# Finished alignment, need to train and test models
+for gender in m f mix
+do
+  train_list=${gender}_train
+  test_list=${gender}_test
+  exp_dir=exp/gender_${gender}
+  data_dir=data/train_${gender}
+  algn_dir=${orig_model}_${gender}_ali
+
+# need second loop for test gender (data/test_f)
+# need to align lang_wsj_test_bg??
+#  ./utils/mkgraph.sh data/lang_wsj_test_bg ${exp_dir} ${exp_dir}/graph
+#  ./steps/decode.sh --nj 4 ${exp_dir}/graph data/test_words ${exp_dir}/decode_test
+#  ./local/score_words.sh data/test_words ${exp_dir}/graph ${exp_dir}/decode_test
+#  more ${exp_dir}/decode_test/scoring_kaldi/best_wer >> my-local/gender_results
 done
