bdd3ec98675b56eae1129be0d28f872f775a482f
jchiiim <jennychim@gmail.com>
2018-03-08 21:20:10 +0000

    Updated file paths

--- exp_mono_t2_WIP.sh
+++ exp_mono_t2_WIP.sh
@@ -3,21 +3,21 @@
 # Since we are training a GMM-HMM system, we chose PLP features.
 
 # Created 'dummy' plp.conf (1-line: --sample-freq=16000)
-# Created separate directories for plp (train/test_words_plp)
+# Coped training/testing data to separate directories for plp (train/test_words_plp)
 
 # Make PLP features
 for dir in train_words_plp test_words_plp
 do
-  steps/make_plp.sh data/$dir exp/make_plp/$dir plp
+  ./steps/make_plp.sh ./data/$dir ./exp/make_plp/$dir plp
 done
 
 # Train monophone model on PLP features
-steps/train_mono.sh --nj 4 data/train_words_plp data/lang_wsj exp/plp
+./steps/train_mono.sh --nj 4 ./data/train_words_plp ./data/lang_wsj ./exp/plp
 
 # Decode
-utils/mkgraph.sh --mono data/lang_wsj_test_bg exp/plp exp/plp/graph
-steps/decode.sh --nj 4 exp/plp/graph data/test_words_plp exp/plp/decode_test
-local/score_words.sh data/test_words_plp exp/plp/graph exp/plp/decode_test
-more exp/plp/decode_test/scoring_kaldi/best_wer
+./utils/mkgraph.sh --mono ./data/lang_wsj_test_bg ./exp/plp ./exp/plp/graph
+./steps/decode.sh --nj 4 ./exp/plp/graph ./data/test_words_plp ./exp/plp/decode_test
+./local/score_words.sh ./data/test_words_plp ./exp/plp/graph ./exp/plp/decode_test
+more ./exp/plp/decode_test/scoring_kaldi/best_wer
 
 # this produced a WER of 83.64
