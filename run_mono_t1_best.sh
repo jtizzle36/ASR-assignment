66c681251306a5f2d7bf28a3495c2fa92d5ea37f
jchiiim <jennychim@gmail.com>
2018-03-12 09:41:13 +0000

    Update run_mono_t1_best.sh

--- run_mono_t1_best.sh
+++ run_mono_t1_best.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Decode, score, and display WER of monophone with optimal number of gaussian components
 
-best_totgauss=1000
+best_totgauss=9000
 steps/decode.sh --nj 4 exp/mono${best_totgauss}/graph data/test_words exp/mono${best_totgauss}/decode_test
 local/score_words.sh data/test_words exp/${best_totgauss}/graph exp/mono${best_totgauss}/decode_test/
 more exp/mono${best_totgauss}/decode_test/scoring_kaldi/best_wer
