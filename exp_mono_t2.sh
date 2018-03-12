5187cd106a56d2b52e88f122ab333f547ea7bd97
jchiiim <jennychim@gmail.com>
2018-03-12 18:19:50 +0000

    Update exp_mono_t2.sh

--- exp_mono_t2.sh
+++ exp_mono_t2.sh
@@ -6,22 +6,20 @@
 # Coped training/testing data to separate directories for plp (train/test_words_plp)
 
 # Make PLP features
-#for dir in train_words_plp test_words_plp
-#do
-#  ./steps/make_plp.sh ./data/$dir ./exp/make_plp/$dir plp
-#done
+for dir in train_words_plp test_words_plp; do
+  ./steps/make_plp.sh ./data/$dir ./exp/make_plp/$dir plp
+done
 
 exp_dir=./exp/plp
 
 # Compute CMVN stats and check feature dimensions
-# for dir in train_words_plp test_words_plp
-# do
-#  ./steps/compute_cmvn_stats.sh data/${dir}
-#  feat-to-dim scp:data/${dir}/feats.scp -
-# done
+for dir in train_words_plp test_words_plp; do
+  ./steps/compute_cmvn_stats.sh data/${dir}
+  #feat-to-dim scp:data/${dir}/feats.scp -
+done
 
 # Train monophone model on PLP features
-# ./steps/train_mono.sh --nj 4 --totgauss 9000 ./data/train_words_plp ./data/lang_wsj ${exp_dir}
+./steps/train_mono.sh --nj 4 --totgauss 9000 ./data/train_words_plp ./data/lang_wsj ${exp_dir}
 
 # Decode
 ./utils/mkgraph.sh --mono ./data/lang_wsj_test_bg ${exp_dir} ${exp_dir}/graph
